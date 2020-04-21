f29a4ea53affcf820b6fa996252295cbb6afb2f7
George Mihoc <george.d2000@gmail.com>
2020-04-21 17:31:32 +0300

    Initial Commit

---
+++ Bitcoin price/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin price
+//
+//  Created by George on 21/04/2020.
+//  Copyright © 2020 George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
